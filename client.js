a05ace7a446f4effadc3d3415718bb9ffd77fc09
Tausif Khan <tausifkhan4171@gmail.com>
2024-03-21 00:11:40 +0530

    sending message from client to server 🎉

--- client.js
+++ client.js
@@ -1,5 +1,14 @@
 const socket = io('http://localhost:4000')
 
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
 socket.on('send-message', data => {
     console.log(data)
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    socket.emit('send-chat-message', messageInput.value)
+    messageInput.value = ''
 })
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const io = require('socket.io')(4000)
 
 io.on('connection', socket => {
+    console.log('a user connected')
     socket.emit('send-message', 'Hello, world')
+    socket.on('send-chat-message', message => {
+        console.log(message)
+    })
 })
