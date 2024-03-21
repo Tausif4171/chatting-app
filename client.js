bb63b79adaffb2044cf6bee2b0ee164f6d77863d
Tausif Khan <tausifkhan4171@gmail.com>
2024-03-21 20:41:37 +0530

    Displaying the names of users who are just connected.

--- client.js
+++ client.js
@@ -1,10 +1,22 @@
 const socket = io('http://localhost:4000')
 
 const messageForm = document.getElementById('send-container')
+const messageContainer = document.getElementById('message-container')
 const messageInput = document.getElementById('message-input')
+
+const name = prompt('What is your name?')
+appendMessage('You joined')
+
+socket.emit('new-user', name)
 
 socket.on('send-message', data => {
     console.log(data)
+    appendMessage(data)
+})
+
+socket.on('user-connected', data => {
+    console.log(data)
+    appendMessage(data)
 })
 
 messageForm.addEventListener('submit', e => {
@@ -12,3 +24,9 @@ messageForm.addEventListener('submit', e => {
     socket.emit('send-chat-message', messageInput.value)
     messageInput.value = ''
 })
+
+function appendMessage(message) {
+    const data = document.createElement('p')
+    data.textContent = message
+    messageContainer.appendChild(data)
+}
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const io = require('socket.io')(4000)
 
+const users = {}
+console.log({ users })
+
 io.on('connection', socket => {
     console.log('a user connected')
     // socket.emit('send-message', 'Hello, world')
     socket.on('send-chat-message', message => {
         console.log(message)
         socket.broadcast.emit('send-message', message)
+    })
+
+    socket.on('new-user', user => {
+        users[socket.id] = user
+        socket.broadcast.emit('user-connected', user)
     })
 })
